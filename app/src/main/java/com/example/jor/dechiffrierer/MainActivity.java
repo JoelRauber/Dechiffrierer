be994ffb6e048cae6c7712574d1b161314c7422e
JoelRauber <joelarauber@hotmail.com>
2018-09-19 15:35:48 +0200

    ADDED: Kamera

--- app/src/main/java/com/example/jor/dechiffrierer/MainActivity.java
+++ app/src/main/java/com/example/jor/dechiffrierer/MainActivity.java
@@ -1,13 +1,131 @@
 package com.example.jor.dechiffrierer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+    private String mCurrentPhotoPath = null;
+
+    private Uri contentUri;
+
+    private Bitmap bitmap;
+
+    private Uri photoURI;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            dispatchTakePictureIntent();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        galleryAddPic();
+
+        //readImageFile(this.photoURI);
+    }
+
+    private void dispatchTakePictureIntent() throws Exception {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try{
+                photoFile = createImageFile();
+            }
+            catch (IOException ex) {
+                throw new Exception("Error while creating File!");
+            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            if (photoFile != null) {
+                this.photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, this.photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private Bitmap readImageFile(Uri imageUri) {
+        File file = new File(imageUri.getPath());
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+            this.bitmap = BitmapFactory.decodeStream(is);
+            return this.bitmap;
+        } catch (FileNotFoundException e) {
+            Log.e("DECODER", "Could not find image file", e);
+            return null;
+        } finally {
+            if(is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        this.mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(this.mCurrentPhotoPath);
+        this.contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(this.contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M){
+            final Uri data = intent.getData();
+            final File file = new File(data.getPath());
+            // now you can upload your image file
+        }else{
+            // in android version lower than M your method must work
+        }
     }
 }
