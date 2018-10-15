b14b9126263c2738bd8583c0a1a5b7c22c4027fe
JoelRauber <joelarauber@hotmail.com>
2018-10-15 08:49:10 +0200

    ADDED: Kammer & Dechiffrieren -> Save full-size photo muss noch gemacht werden

--- app/src/main/java/com/example/jor/dechiffrierer/MainActivity.java
+++ app/src/main/java/com/example/jor/dechiffrierer/MainActivity.java
@@ -1,155 +1,90 @@
 package com.example.jor.dechiffrierer;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-import java.io.Console;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+public class MainActivity extends AppCompatActivity
+    {
+    private static final int SCAN_QR_CODE_REQUEST_CODE;
 
-import static com.sun.activation.registries.LogSupport.log;
-import static java.awt.SystemColor.menu;
+    @SuppressLint("StaticFieldLeak")
+    private static ImageView imgTakenPic;
 
-public class MainActivity extends AppCompatActivity {
+    private static final int CAM_REQUEST;
 
-    private static final int REQUEST_TAKE_PHOTO = 1;
-
-    private String mCurrentPhotoPath = null;
-
-    private Uri contentUri;
-
-    private Bitmap bitmap;
-
-    private Uri photoURI;
-
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    static
+    {
+        SCAN_QR_CODE_REQUEST_CODE = 0;
+        CAM_REQUEST = 1313;
+    }
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            dispatchTakePictureIntent();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Button btnPicture;
+        btnPicture = findViewById(R.id.btnTakePicture);
 
-        galleryAddPic();
+        Button btnDechiffrieren;
+        btnDechiffrieren = findViewById(R.id.btnDechiffrieren);
 
-        //readImageFile(this.photoURI);
-    }
+        imgTakenPic = findViewById(R.id.imageView);
 
-    private void dispatchTakePictureIntent() throws Exception {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-            }
-            catch (IOException ex) {
-                throw new Exception("Error while creating File!");
-            }
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        btnDechiffrieren.setOnClickListener(new btnDechiffrierenClicker(imgTakenPic));
+        btnDechiffrieren.setVisibility(View.GONE);
 
-            if (photoFile != null) {
-                this.photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, this.photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-    private Bitmap readImageFile(Uri imageUri) {
-        File file = new File(imageUri.getPath());
-        InputStream is = null;
-        try {
-            is = new FileInputStream(file);
-            this.bitmap = BitmapFactory.decodeStream(is);
-            return this.bitmap;
-        } catch (FileNotFoundException e) {
-            Log.e("DECODER", "Could not find image file", e);
-            return null;
-        } finally {
-            if(is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-        this.mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(this.mCurrentPhotoPath);
-        this.contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(this.contentUri);
-        this.sendBroadcast(mediaScanIntent);
+        btnPicture.setOnClickListener(new btnTakePhotoClicker(btnDechiffrieren));
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == SCAN_QR_CODE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (requestCode == SCAN_QR_CODE_REQUEST_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
                 String logMsg = intent.getStringExtra("SCAN_RESULT");
                 log(logMsg);
             }
-        }else{
+        }
+        else
+            {
             super.onActivityResult(requestCode, resultCode, intent);
-            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M){
-                final Uri data = intent.getData();
-                final File file = new File(data.getPath());
-                // now you can upload your image file
-            }else{
-                // in android version lower than M your method must work
+            if(requestCode == CAM_REQUEST) {
+                Bitmap bitmap = (Bitmap) Objects.requireNonNull(intent.getExtras()).get("data");
+                imgTakenPic.setImageBitmap(bitmap);
             }
         }
     }
 
-    private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuItem menuItem = menu.add("Log");
-        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
+            public boolean onMenuItemClick(MenuItem item)
+            {
                 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                 intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
                 startActivityForResult(intent, SCAN_QR_CODE_REQUEST_CODE);
@@ -159,13 +94,82 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void log(String qrCode) {
+    class btnTakePhotoClicker implements Button.OnClickListener
+    {
+        Button button;
+
+        public btnTakePhotoClicker(Button button)
+        {
+            this.button = button;
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAM_REQUEST);
+            button.setVisibility(View.VISIBLE);
+        }
+    }
+
+    class btnDechiffrierenClicker implements Button.OnClickListener
+    {
+        private ImageView imageView;
+        public btnDechiffrierenClicker(ImageView imageView)
+        {
+            this.imageView = imageView;
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            Bitmap bitmap =((BitmapDrawable)imageView.getDrawable()).getBitmap();
+            applyFilter(bitmap);
+            imageView.setImageBitmap(bitmap);
+        }
+
+        private Bitmap applyFilter(Bitmap bitmap)
+        {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int[] data = new int[width * height];
+
+            bitmap.getPixels(data, 0, width, 0, 0, width, height);
+
+            ArrayList<Integer> pixelsAR = new ArrayList( );
+            for (int pixel :data)
+            {
+                pixel = Color.red(pixel);
+                pixelsAR.add(pixel);
+            }
+
+            int[] pixels = convertIntegers(pixelsAR);
+
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+            return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
+        }
+
+        public int[] convertIntegers(List<Integer> integers)
+        {
+            int[] ret = new int[integers.size()];
+            for (int i=0; i < ret.length; i++)
+            {
+                ret[i] = integers.get(i).intValue();
+            }
+            return ret;
+        }
+    }
+
+    private void log(String qrCode)
+    {
         Intent intent = new Intent("ch.appquest.intent.LOG");
-        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty())
+        {
             Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
             return;
         }
-        // Achtung, je nach App wird etwas anderes eingetragen
+
         String logmessage = "{\n" + "  \"task\": \"Metalldetektor\",\n" + "  \"solution\": \" "+ qrCode+" \"\n" +"}";
         intent.putExtra("ch.appquest.logmessage", logmessage);
         startActivity(intent);
